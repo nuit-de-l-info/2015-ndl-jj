827a208da3dcdf7c58ca79cd8fa931f227c63232
PierreChaumont <pierre.chaumont31@gmail.com>
2015-12-03 22:10:42 +0100

    feat: ajout de la vue detail crise #3

--- themes/ninfo/js/script.js
+++ themes/ninfo/js/script.js
@@ -1,3 +1,22 @@
 jQuery(document).ready(function($) {
 	
+
+function initMap(latitude, longitude) {
+  // Create a map object and specify the DOM element for display.
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: latitude, lng: longitude},
+    scrollwheel: false,
+    zoom: 8
+  });
+}
+
+$(document).ready(function(){
+    //FANCYBOX
+    //https://github.com/fancyapps/fancyBox
+    $(".fancybox").fancybox({
+        openEffect: "none",
+        closeEffect: "none"
+    });
+});
+
 });
---
+++ themes/ninfo/views/views/detail_crise/detail_crise.php
@@ -0,0 +1,77 @@
+<div class="container">
+	<div class="row">
+		<h1>Détail de la crise #id_crise</h1>
+		<div class="row container-fluid">
+			<div class="col-xs-4">Nom</div>
+			<div class="col-xs-4">#</div>
+			<div class="col-xs-4">Catégorie</div>
+			<div class="col-xs-12 form-group">
+				<label for="description">Description</label>
+				<textarea id="description" class="form-control" value="" disabled></textarea>
+			</div>
+		</div>
+		<div class="row container-fluid">	
+		  <!-- récupérer informations facebook, ... -->
+		  <div class="col-xs-6">Commentaires Facebook, Twitter</div>
+		  <!-- api google ? -->
+		  <div class="col-xs-6" id="map">Carte</div>
+		</div>
+		<div class="row container-fluid">
+			<div class="col-xs-12">Gallerie
+				<div class='list-group gallery'>
+		            <div class='col-sm-4 col-xs-6 col-md-3 col-lg-3'>
+		                <a class="thumbnail fancybox" rel="ligthbox" href="http://placehold.it/300x320.png">
+		                    <img class="img-responsive" alt="" src="http://placehold.it/320x320" />
+		                    <div class='text-right'>
+		                        <small class='text-muted'>Image Title</small>
+		                    </div> <!-- text-right / end -->
+		                </a>
+		            </div> <!-- col-6 / end -->
+		            <div class='col-sm-4 col-xs-6 col-md-3 col-lg-3'>
+		                <a class="thumbnail fancybox" rel="ligthbox" href="http://placehold.it/300x320.png">
+		                    <img class="img-responsive" alt="" src="http://placehold.it/320x320" />
+		                    <div class='text-right'>
+		                        <small class='text-muted'>Image Title</small>
+		                    </div> <!-- text-right / end -->
+		                </a>
+		            </div> <!-- col-6 / end -->
+		            <div class='col-sm-4 col-xs-6 col-md-3 col-lg-3'>
+		                <a class="thumbnail fancybox" rel="ligthbox" href="http://placehold.it/300x320.png">
+		                    <img class="img-responsive" alt="" src="http://placehold.it/320x320" />
+		                    <div class='text-right'>
+		                        <small class='text-muted'>Image Title</small>
+		                    </div> <!-- text-right / end -->
+		                </a>
+		            </div> <!-- col-6 / end -->
+		            <div class='col-sm-4 col-xs-6 col-md-3 col-lg-3'>
+		                <a class="thumbnail fancybox" rel="ligthbox" href="http://placehold.it/300x320.png">
+		                    <img class="img-responsive" alt="" src="http://placehold.it/320x320" />
+		                    <div class='text-right'>
+		                        <small class='text-muted'>Image Title</small>
+		                    </div> <!-- text-right / end -->
+		                </a>
+		            </div> <!-- col-6 / end -->
+		            <div class='col-sm-4 col-xs-6 col-md-3 col-lg-3'>
+		                <a class="thumbnail fancybox" rel="ligthbox" href="http://placehold.it/300x320.png">
+		                    <img class="img-responsive" alt="" src="http://placehold.it/320x320" />
+		                    <div class='text-right'>
+		                        <small class='text-muted'>Image Title</small>
+		                    </div> <!-- text-right / end -->
+		                </a>
+		            </div> <!-- col-6 / end -->
+		            <div class='col-sm-4 col-xs-6 col-md-3 col-lg-3'>
+		                <a class="thumbnail fancybox" rel="ligthbox" href="http://placehold.it/300x320.png">
+		                    <img class="img-responsive" alt="" src="http://placehold.it/320x320" />
+		                    <div class='text-right'>
+		                        <small class='text-muted'>Image Title</small>
+		                    </div> <!-- text-right / end -->
+		                </a>
+		            </div> <!-- col-6 / end -->
+		        </div>
+			</div>
+		</div>
+		<button class="btn btn-default">Commenter</button>
+	</div>
+</div>
+
+<script src="//cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.js"></script>
