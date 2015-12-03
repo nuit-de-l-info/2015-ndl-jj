7f93aaf2aa2623cad67360aa04972877ef1f9858
Arthur Gorjux <arthurgorjux@gmail.com>
2015-12-03 20:48:46 +0100

    Feat: Ajout données de test pour formulaire (select) Issue #4

--- application/controllers/Creation_crise.php
+++ application/controllers/Creation_crise.php
@@ -6,6 +6,8 @@ class Creation_crise extends MY_Controller {
 
     public function index()
     {
+        // @todo : add get data from database (table categorie)
+        $this->data->categories = array('attentat', 'séisme', 'ouragan', 'thibault');
     	$this->template->set_layout('default')
             ->build('views/creation_crise/add', $this->data);
     }
