7b157b4c11ca2e822ebef224f20b3995f2901fe9
Lucas Fantinel <lucas.fantinel@gmail.com>
2015-12-04 04:48:30 +0100

    feat: ajout du jeu This close #16

--- themes/ninfo/views/partials/header.php
+++ themes/ninfo/views/partials/header.php
@@ -3,6 +3,7 @@
 		<div class="container">
 			<div class="navbar-header"><a class="navbar-brand" href="/" accesskey="3">InfoCrises</a></div>
 			<ul class="nav navbar-nav navbar-right">
+				<li><a href="/ninfo-game">The impossible game</a></li>
 				<li><a href="/index.php/admin/choisir-crise-a-valider">Administration</a></li>
 				<li><a href="/index.php/connexion">Connexion</a></li>
 			</ul>
