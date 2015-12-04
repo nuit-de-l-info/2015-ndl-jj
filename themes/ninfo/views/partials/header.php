e879dcce6db60bb7094f907dd1a540666e538e5a
Lucas Fantinel <lucas.fantinel@gmail.com>
2015-12-04 04:21:10 +0100

    feat:rajout du descriptif des accesskey This resolve #15

--- themes/ninfo/views/partials/header.php
+++ themes/ninfo/views/partials/header.php
@@ -1,7 +1,7 @@
 <header id="header" class="navbar navbar-fixed-top">	
 	<nav class="collapse navbar-inverse navbar-collapse">
 		<div class="container">
-			<div class="navbar-header"><a class="navbar-brand" href="/">InfoCrises</a></div>
+			<div class="navbar-header"><a class="navbar-brand" href="/" accesskey="3">InfoCrises</a></div>
 			<ul class="nav navbar-nav navbar-right">
 				<li><a href="/index.php/admin/choisir-crise-a-valider">Administration</a></li>
 				<li><a href="/index.php/connexion">Connexion</a></li>
