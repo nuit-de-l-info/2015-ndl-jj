4329d177d338fb6d7a5087ac7fee78a4bfe7fe4a
Thomas Laumondais <thomas.laumondais@gmail.com>
2015-12-04 03:43:34 +0100

    Suite refonte graphique

--- themes/ninfo/views/partials/footer.php
+++ themes/ninfo/views/partials/footer.php
@@ -1,7 +1,7 @@
 <footer id="footer">
 		<div class="row">
 			<div class="col-xs-12">
-				<p><i>Nuit de l'info 2015</i></p>
+				<p class="text-muted"><i>Nuit de l'info 2015</i></p>
 			</div>
 		</div>
 </footer>
